23936d523ebce81d61fbc894ce4123ba6414a19e
geof.hoffman <geoff.c.137@gmail.com>
2020-04-24 13:19:53 -0700

    drectory component

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+<<<<<<< HEAD
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+=======
+import Main from './components/MainComponent';
+
+
+export default function App() {
+  return (
+   <Main />
+  );
+}
+
+
+>>>>>>> 15edb513c487406126e4d67d784e37786c8b6e84
---
+++ components/CampisteInfoComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+function RenderCampsite({campsite}) {
+    if (campsite)
+    return( 
+        <Card
+            featuredTitle={campsite.name}
+            image={require('./images/react-lake.jpg')}>
+            <Text style={{margin:10}}>
+                {campsite.description}
+            </Text>
+        </Card>
+        );
+    }
+    return <view />;
+}
+
+function CampisteInfo(props){
+    return <RenderCampsite campsite= {props.campsite} />
+}
+export default CampisteInfo;
