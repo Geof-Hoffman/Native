af40dcc4383d80e9b451733ee703a94970691eb5
geof.hoffman <geoff.c.137@gmail.com>
2020-04-30 16:36:03 -0400

    Setting up Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text,  FlatList  } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 //signals which part of state is needed so entire state doesn't have to b mapped. 
 const mapStateToProps = state => {
@@ -27,28 +28,51 @@ class About extends Component {
         title: 'About Us'
     };
 
-    render() {
-        const renderPartner= ({item}) => {
+        render() {
+            const renderPartner = ({item}) => {
+                return (
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    />
+                );
+            };
+    
+            if (this.props.partners.isLoading) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Loading />
+                        </Card>
+                    </ScrollView>
+                );
+            }
+            if (this.props.partners.errMess) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </ScrollView>
+                );
+            }
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                />
+                <ScrollView>
+                    <Mission /> 
+                    <Card>
+                        <FlatList
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={(item) => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>  
             );
-        };
-       return (
-            <ScrollView>
-              <Mission /> 
-              <Card>
-              <FlatList
-                data={this.props.partners.partners}
-                renderItem={renderPartner}
-                keyExtractor={(item) => item.id.toString()}
-            />
-            </Card>
-              </ScrollView>  
-        );
     }
 }
 
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent'
 
 const mapStateToProps = state => {
     return {
@@ -21,6 +22,7 @@ class Directory extends Component {
 
     render() {
         const { navigate } = this.props.navigation;
+        
         const renderDirectoryItem = ({item}) => {
             return (
                 <Tile
@@ -33,8 +35,18 @@ class Directory extends Component {
             );
         };
 
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+               </View>
+            );
+        }
         return (
-            <FlatList
+            <FlatList       
                 data={this.props.campsites.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
